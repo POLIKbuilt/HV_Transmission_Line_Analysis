ac7cfba1b40c5602016174f7ec462d6593b3387d
POLIKbuilt <danilkach73@gmail.com>
2025-09-29 21:01:11 +0200

    correcting new class

--- current_demand.py
+++ current_demand.py
@@ -3,11 +3,11 @@ import numpy as np
 from scipy.constants import sigma, g
 
 class Demand_Current:
-    def teplo_ziarenia(self, I_s, d):
+    def teplo_ziarenia(I_s, d):
         Ps = 0.5 * I_s * (d / 1000)
         return Ps
 
-    def teplo_konvekcii(self, t_s, t_a, h, v, d, d_s):
+    def teplo_konvekcii(t_s, t_a, h, v, d, d_s):
         t_f = 0.5 * (t_s + t_a)
         v_f = 0.0000132 + 0.000000095 * t_f
         lambda_f = 0.0242 + 0.000072 * t_f
@@ -56,12 +56,11 @@ class Demand_Current:
         Pc = np.pi * lambda_f * ( t_s - t_a ) * max(Nu_nat,Nu_corr,Nu45)
         return Pc
 
-    def teplo_radiation(self, d, t_a, t_s):
+    def teplo_radiation(d, t_a, t_s):
         Pr = np.pi * (d / 1000) * sigma  * 0.5 * ( (t_s + 273 ) ** 4 - (t_a + 273) ** 4)
         return  Pr
 
-    def ampacita(self, Pc, Pr, Ps, Rdc20):
-        Rdc20 = 0.0608
+    def ampacita(Pc, Pr, Ps, Rdc20):
         alpha_R = 0.00403
         k_acdc = 1.080
         Rdc80 = (Rdc20 * (1 - alpha_R * (80 - 20) )) / 1000
@@ -70,6 +69,7 @@ class Demand_Current:
         return I_dov
 
 if __name__ == "__main__":
+    Rdc20 = 0.0608
     Pc = Demand_Current.teplo_konvekcii(80, 35, 400, 0.5, 30.2, 3.35)
     Pr = Demand_Current.teplo_radiation(30.2, 35, 80)
     Ps = Demand_Current.teplo_ziarenia(1000, 30.2)
