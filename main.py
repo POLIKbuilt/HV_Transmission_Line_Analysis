18e9e70ebedcef950ff2a2d84d3e8ffc9075fb92
POLIKbuilt <danilkach73@gmail.com>
2025-10-06 09:02:13 +0200

    input edited

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ file_path = os.path.join(base_dir, 'data.csv')
 # 'D:\IT\Transmission_Line_Analysis_VEV'
 
 def load_terrain(file_path):
-    with open(file_path, 'r') as csv:
-        land_data = csv.reader()
-
+    # csv read
+    with open(file_path, newline="") as csvfile:
+        reader = csv.reader(csvfile)
+    return reader
 
 
 if __name__ == "__main__":
-    # csv read
-    with open("data/data.csv", newline="") as csvfile:
-        reader = csv.reader(csvfile)
+    file_path = "data/data.csv"
     # project data input
     T_env_max = 33 # C degrees
     V_min = 0.4 # m/s
     I_sun = 1097 # w/m
     k_abs = 0.5
     k_emis = 0.5
+    h_sea = 400
     # cable data sheet input
     T_cab_max = 80 # C degree
     h_env = 12 # m
@@ -30,6 +30,9 @@ if __name__ == "__main__":
     ampacita_min = 2000 # A
 
     Rdc20 = 0.0608
+
+    # main run
+    terrain_data = load_terrain(file_path)
     Pc = Demand_Current.teplo_konvekcii(80, 35, 400, 0.5, 30.2, 3.35)
     Pr = Demand_Current.teplo_radiation(30.2, 35, 80)
     Ps = Demand_Current.teplo_ziarenia(1000, 30.2)
