844ffc1424db369520d578c1e3360b1f26a2466a
POLIKbuilt <danilkach73@gmail.com>
2025-10-06 09:37:01 +0200

    fixed terrain ploting

--- main.py
+++ main.py
@@ -12,15 +12,16 @@ def load_terrain(file_path):
     # csv read
     ter_X = []
     ter_Y = []
-    with open(file_path, newline="") as csvfile:
+    with open(file_path, newline="", encoding="utf-8-sig") as csvfile:
         reader = csv.DictReader(csvfile)
+        print(reader.fieldnames)
         for row in reader:
+            print(row["X"], row["Y"])
             ter_X.append(float(row["X"]))
             ter_Y.append(float(row["Y"]))
         plt.plot(ter_X,ter_Y)
         plt.title("Terrain")
         plt.show()
-    return reader
 
 
 if __name__ == "__main__":
