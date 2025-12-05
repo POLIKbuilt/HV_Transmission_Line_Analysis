04de59244f11de8bbe29e31d57fd5af7e7f2bbda
Polik <34038570+POLIKbuilt@users.noreply.github.com>
2025-12-05 11:09:36 +0100

    Refactor vibration control equations and logic
    Refactor vibration control logic to streamline calculations and enhance readability.

--- vibration_contol.py
+++ vibration_contol.py
@@ -14,33 +14,58 @@ class VibrationControl:
         self.S = S
         self.terrain_type = terrain_type
 
-def vibration_control_equations(self):
-        self.y_coor = (self.v_rozpatie * self.d * 1e-3) / self.g_c
+    def vibration_control_equations(self):
+            self.y_coor = (self.v_rozpatie * self.d * 1e-3) / self.g_c
+    
+            self.T_0 = self.v_sigma_h1[3] * self.S
+            self.x_coor = self.T_0 / self.g_c
+    
+            if self.terrain_type == 1:
+                self.Eq_vib = (1.3 * 10 ** 27) / (self.T_0 / self.g_c) ** 8.3
+                self.c_vib = 1000
+            elif self.terrain_type == 2:
+                self.Eq_vib = (5.4 * 10 ** 27) / (self.T_0 / self.g_c) ** 8.4
+                self.c_vib = 1125
+            elif self.terrain_type == 3:
+                self.Eq_vib = (1.3 * 10 ** 28) / (self.T_0 / self.g_c) ** 8.4
+                self.c_vib = 1225
+            elif self.terrain_type == 4:
+                self.Eq_vib = (1.1 * 10 ** 29) / (self.T_0 / self.g_c) ** 8.6
+                self.c_vib = 1425
+    
+            # Условие
+            if self.x_coor <= self.c_vib:
+                self.oblast = 1
+            else:
+                self.oblast = 0
+                for i in range(len(self.v_rozpatie)):
+                    if (self.y_coor[i] <= 1.5) and (self.y_coor[i] <= self.Eq_vib):
+                        self.oblast = 2
+                    elif (self.y_coor[i] > 1.5) and (self.y_coor[i] > self.Eq_vib):
+                        self.oblast = 3
+            return self.oblast
+    
+    def compute_heights(self):
+        self.c_80 = self.montazne_tabulky_konecne[5, -1]
 
-        self.T_0 = self.v_sigma_h1[3] * self.S
-        self.x_coor = self.T_0 / self.g_c
+        n = len(self.v_rozpatie)
+        self.v_a_ciarka = np.zeros(n)
+        self.v_h_par = np.zeros(n)
+        self.v_x_min = np.zeros(n)
+        self.j_x = np.zeros(n)
 
-        if self.terrain_type == 1:
-            self.Eq_vib = (1.3 * 10 ** 27) / (self.T_0 / self.g_c) ** 8.3
-            self.c_vib = 1000
-        elif self.terrain_type == 2:
-            self.Eq_vib = (5.4 * 10 ** 27) / (self.T_0 / self.g_c) ** 8.4
-            self.c_vib = 1125
-        elif self.terrain_type == 3:
-            self.Eq_vib = (1.3 * 10 ** 28) / (self.T_0 / self.g_c) ** 8.4
-            self.c_vib = 1225
-        elif self.terrain_type == 4:
-            self.Eq_vib = (1.1 * 10 ** 29) / (self.T_0 / self.g_c) ** 8.6
-            self.c_vib = 1425
+        for i in range(n):
+            if i + 1 < len(self.v_h_alt):
+                self.v_a_ciarka[i] = self.v_rozpatie[i] - (asinh(((self.v_h_alt[i + 1] + self.v_h_con[i + 1]) - (self.v_h_alt[i] + self.v_h_con[i])) / (2 * self.c_80 * np.sinh(self.v_rozpatie[i] / (2 * self.c_80)))) + self.v_rozpatie[i] / (2 * self.c_80)) * self.c_80
+                self.v_h_par[i] = ((self.v_h_alt[i] + self.v_h_con[i]) - self.c_80 * np.cosh(self.v_a_ciarka[i] / self.c_80) + self.c_80)
 
-        # Условие
-        if self.x_coor <= self.c_vib:
-            self.oblast = 1
-        else:
-            self.oblast = 0
-            for i in range(len(self.v_rozpatie)):
-                if (self.y_coor[i] <= 1.5) and (self.y_coor[i] <= self.Eq_vib):
-                    self.oblast = 2
-                elif (self.y_coor[i] > 1.5) and (self.y_coor[i] > self.Eq_vib):
-                    self.oblast = 3
-        return self.oblast
+        for i in range(n):
+            if i + 1 < len(self.v_h_alt):
+                pom_a = (self.v_h_alt[i + 1] - self.v_h_alt[i]) / self.v_rozpatie[i]
+                self.v_x_min[i] = log(pom_a + np.sqrt(pom_a ** 2 + 1)) * self.c_80 + self.v_a_ciarka[i]
+
+        for i in range(n):
+            if i + 1 < len(self.v_h_alt):
+                self.j_x[i] = (
+                    self.c_80 * (np.cosh((self.v_x_min[i] - self.v_a_ciarka[i]) / self.c_80)) - self.c_80 + self.v_h_par[i] - (self.v_x_min[i] * (self.v_h_alt[i + 1] - self.v_h_alt[i]) / self.v_rozpatie[i] + self.v_h_alt[i]))
+
