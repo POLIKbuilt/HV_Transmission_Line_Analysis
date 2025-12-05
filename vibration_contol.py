1356675b3e6d68a385a66e19a2aec498686e2a1a
Polik <34038570+POLIKbuilt@users.noreply.github.com>
2025-12-05 11:08:00 +0100

    Add VibrationControl class with initialization parameters

--- vibration_contol.py
+++ vibration_contol.py
@@ -1,3 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import asinh, log
+
+class VibrationControl:
+    def __init__(self, montazne_tabulky_konecne, v_rozpatie, v_sigma_h1, v_h_alt, v_h_con, d, g_c, S, terrain_type):
+        self.montazne_tabulky_konecne = montazne_tabulky_konecne
+        self.v_rozpatie = v_rozpatie
+        self.v_sigma_h1 = v_sigma_h1
+        self.v_h_alt = v_h_alt
+        self.v_h_con = v_h_con
+        self.d = d
+        self.g_c = g_c
+        self.S = S
+        self.terrain_type = terrain_type
