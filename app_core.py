a9f5238dfa7bdac1edc23c5aacc307c366b8a7a2
polik-t495 <danilkach73@gmail.com>
2026-09-23 14:05:56 +0200

    sets impl

--- app_core.py
+++ app_core.py
@@ -1,12 +1,22 @@
 import sys
 from PyQt5.QtWidgets import QApplication,QMainWindow, QLabel
+from app_settings import load_settings, save_settings
 
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.settings = load_settings()
+
+
         self.setWindowTitle("Transmission Analyzer")
-        self.setGeometry(300, 300, 300, 300)
-        self.setCentralWidget(QLabel("Shit!"))
+        self.setGeometry(300, 300, self.settings["window_width"], self.settings["window_height"])
+        self.setCentralWidget(QLabel(f"Theme: {self.settings['theme']}"))
+
+def closeEvent(self, event):
+    self.settings["window_width"] = self.width()
+    self.settings["window_height"] = self.height()
+    save_settings(self.settings)
+    event.accept()
 
 app = QApplication(sys.argv)
 window = App()
